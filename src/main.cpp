9312ac4f9641d568d0e45117dd51ddee9024cc53
Lorenx03 <lorenx.gt@gmail.com>
2024-10-07 04:41:29 +0200

    minimal

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <Arduino.h>
 #include <DNSServer.h>
 #ifdef ESP32
-#include <AsyncTCP.h>
 #include <WiFi.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #endif
-#include "ESPAsyncWebServer.h"
-#include "SPIFFS.h"
-#include "webgui.h"
 #include "Automathaus.h"
 
 AsyncWebServer server(80);
@@ -18,10 +14,6 @@ Automathaus automathaus(&automathausWebServer);
 
 const char* ssid = "H3140-74454476";
 const char* password = "Zk7FZEUSuz";
-
-// void notFound(AsyncWebServerRequest *request) {
-//     request->send(404, "text/plain", "Not found");
-// }
 
 void setup() {
     Serial.begin(115200);
@@ -37,13 +29,6 @@ void setup() {
     Serial.println(WiFi.localIP());
 
     automathaus.start();
-
-    // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    //     request->send_P(200, "text/html", INDEX_HTML);
-    // });
-
-    // server.onNotFound(notFound);
-    // server.begin();
 }
 
 void loop() {}
